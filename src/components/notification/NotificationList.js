0ff71e2ac6d2177e5b58f2c4a65fe6c81d031542
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-05-17 19:03:33 +0800

    improving the navbar component and it's child component

--- src/components/notification/NotificationList.js
+++ src/components/notification/NotificationList.js
@@ -1,10 +1,58 @@
-import React from "react";
-import { useNotificationListStyles } from "../../styles";
+import React, {useRef} from "react";
+import {useNotificationListStyles} from "../../styles";
+import Grid from "@material-ui/core/Grid";
+import {defaultNotifications} from "../../data";
+import Avatar from "@material-ui/core/Avatar";
+import {Link} from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import FollowButton from "../shared/FollowButton";
+import useOutsideClick from "@rooks/use-outside-click";
 
-function NotificationList() {
-  useNotificationListStyles();
+function NotificationList({handleHideList}) {
+    const classes = useNotificationListStyles();
+    const listContainerRef = useRef();
+    useOutsideClick(listContainerRef, handleHideList);
+    return (
+        <Grid ref={listContainerRef} className={classes.listContainer} container>
+            {defaultNotifications.map(notification => {
+                const isLike = notification.type === 'like';
+                const isFollow = notification.type === 'follow';
 
-  return <div>NotificationList</div>;
+                return (
+                    <Grid key={notification.id} item className={classes.listItem}>
+                        <div className={classes.listItemWrapper}>
+                            <div className={classes.avatarWrapper}>
+                                <Avatar src={notification.user.profile_image} alt={"User avatar"}/>
+                            </div>
+                            <div className={classes.nameWrapper}>
+                                <Link to={`/${notification.user.username}`}>
+                                    <Typography variant='body1'>
+                                        {notification.user.username}
+                                    </Typography>
+                                </Link>
+
+                                <Typography
+                                    variant='body2'
+                                    color={'textSecondary'}
+                                    className={classes.typography}>
+                                    {isLike && `Likes your photo. 4d`}
+                                    {isFollow && `started following you. 5d`}
+                                </Typography>
+                            </div>
+                        </div>
+                        <div>
+                            {isLike && (
+                                <Link to={`/p/${notification.post.id}`}>
+                                    <Avatar src={notification.post.media} alt={'post cover'}/>
+                                </Link>
+                            )}
+                            {isFollow && <FollowButton/>}
+                        </div>
+                    </Grid>
+                )
+            })}
+        </Grid>
+    );
 }
 
 export default NotificationList;
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,31 +1,212 @@
-import React from "react";
-import { useNavbarStyles } from "../../styles";
-import {AppBar} from "@material-ui/core";
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {RedTooltip, useNavbarStyles, WhiteTooltip} from "../../styles";
+import {AppBar, Fade, Hidden, InputBase, Zoom} from "@material-ui/core";
+import {Link, useHistory} from "react-router-dom";
 import logo from '../../images/logo.png'
-function Navbar() {
-  const classes = useNavbarStyles();
+import {
+    AddIcon,
+    ExploreActiveIcon,
+    ExploreIcon,
+    HomeActiveIcon,
+    HomeIcon,
+    LikeActiveIcon, LikeIcon,
+    LoadingIcon
+} from "../../icons";
+import {defaultCurrentUser, getDefaultUser} from "../../data";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import NotificationTooltip from "../notification/NotificationTooltip";
+import NotificationList from "../notification/NotificationList";
+import {useNProgress} from "@tanem/react-nprogress";
 
-  return (
-      <AppBar className={classes.appBar}>
-        <section className={classes.section}>
-          <Logo />
-        </section>
-      </AppBar>
-  );
+function Navbar({minimalNavbar}) {
+    const classes = useNavbarStyles();
+    const history = useHistory();
+    const path = history.location.pathname;
+    const [isLoadingPage, setLoadingPage] = useState(true);
+
+    useEffect(() => {
+        setLoadingPage(false);
+    }, [path]);
+    return (
+        <>
+            <Progress isAnimating={isLoadingPage} />
+            <AppBar className={classes.appBar}>
+                <section className={classes.section}>
+                    <Logo/>
+                    {!minimalNavbar && (
+                        <>
+                            <Search history={history}/>
+                            <Links path={path}/>
+                        </>
+                    )}
+                </section>
+            </AppBar>
+        </>
+    );
 }
 
 function Logo() {
-  const classes = useNavbarStyles();
-  return (
-      <div className={classes.logoContainer}>
-        <Link to={"/"}>
-          <div className={classes.logoWrapper}>
-            <img src={logo} alt={'logo'} className={classes.logo}/>
-          </div>
-        </Link>
-      </div>
-  )
+    const classes = useNavbarStyles();
+    return (
+        <div className={classes.logoContainer}>
+            <Link to={"/"}>
+                <div className={classes.logoWrapper}>
+                    <img src={logo} alt={'logo'} className={classes.logo}/>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+function Search({history}) {
+    const classes = useNavbarStyles();
+    const [loading, setLoading] = useState(false);
+    const [results, setResults] = useState([]);
+    const [query, setQuery] = useState('');
+
+    const hasResults = Boolean(query) && results.length > 0;
+
+    useEffect(() => {
+        if (!query.trim()) return;
+        setResults(Array.from({length: 5}, () => getDefaultUser()))
+    }, [query]);
+
+    function handleClearInput() {
+        setQuery('');
+    }
+
+    return (
+        <Hidden xsDown>
+            <WhiteTooltip
+                arrow
+                interactive
+                TransitionComponent={Fade}
+                open={hasResults}
+                title={
+                    hasResults && (
+                        <Grid className={classes.resultContainer} container>
+                            {results.map(result => (
+                                <Grid
+                                    onClick={() => {
+                                        history.push(`/${result.username}`);
+                                        handleClearInput()
+                                    }}
+                                    key={result.id}
+                                    item
+                                    className={classes.resultLink}>
+                                    <div className={classes.resultWrapper}>
+                                        <div className={classes.avatarWrapper}>
+                                            <Avatar src={result.profile_image} alt={"user avatar"}/>
+                                        </div>
+                                        <div className={classes.nameWrapper}>
+                                            <Typography
+                                                variant={"body1"}>{result.username}                                            </Typography>
+                                            <Typography variant={"body2"} color={"textSecondary"}>
+                                                {result.name}
+                                            </Typography>
+                                        </div>
+                                    </div>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )
+                }
+            >
+                <InputBase
+                    className={classes.input}
+                    onChange={event => setQuery(event.target.value)}
+                    startAdornment={<span className={classes.searchIcon}/>}
+                    endAdornment={
+                        loading ? (
+                            <LoadingIcon/>
+                        ) : (
+                            <span onClick={handleClearInput} className={classes.clearIcon}/>
+                        )
+                    }
+                    placeholder={'Search'}
+                    value={query}
+                />
+            </WhiteTooltip>
+        </Hidden>
+    )
+}
+
+function Links({path}) {
+    const classes = useNavbarStyles();
+    const [showTooltip, setTooltip] = useState(true);
+    const [showList, setList] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(handleHideTooltip, 5000);
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, []);
+
+    function handleToggleList() {
+        setList(prev => !prev);
+    }
+
+    function handleHideTooltip() {
+        setTooltip(false);
+    }
+
+    function handleHideList() {
+        setList(false);
+    }
+
+    return (
+        <div className={classes.linksContainer}>
+            {showList && <NotificationList handleHideList={handleHideList}/>}
+            <div className={classes.linksWrapper}>
+                <Hidden xsDown>
+                    <AddIcon/>
+                </Hidden>
+                <Link to={"/"}>
+                    {path === "/" ? <HomeActiveIcon/> : <HomeIcon/>}
+                </Link>
+                <Link to={"/explore"}>
+                    {path === "/explore" ? <ExploreActiveIcon/> : <ExploreIcon/>}
+                </Link>
+                <RedTooltip
+                    arrow
+                    open={showTooltip}
+                    onOpen={handleHideTooltip}
+                    TransitionComponent={Zoom}
+                    title={<NotificationTooltip/>}
+                >
+                    <div className={classes.notifications} onClick={handleToggleList}>
+                        {showList ? <LikeActiveIcon/> : <LikeIcon/>}
+                    </div>
+                </RedTooltip>
+                <Link to={`/${defaultCurrentUser.username}`}>
+                    <div className={path === `/${defaultCurrentUser}` ? classes.profileActive : ''}>
+                    </div>
+                    <Avatar src={defaultCurrentUser.profile_image} className={classes.profileImage}/>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+function Progress({isAnimating}) {
+    const classes = useNavbarStyles();
+    const {animationDuration, isFinished, progress} = useNProgress({isAnimating});
+    return (
+        <div className={classes.progressContainer} style={{
+            opacity: isFinished ? 0 : 1,
+            transition: `opacity ${animationDuration}ms linear`
+        }}>
+            <div className={classes.progressBar} style={{
+                marginLeft: `${(-1 + progress) * 100}%`,
+                transition: `margin-left ${animationDuration}ms linear`
+            }}>
+                <div className={classes.progressBackground} />
+            </div>
+        </div>
+    )
 }
 
 export default Navbar;
